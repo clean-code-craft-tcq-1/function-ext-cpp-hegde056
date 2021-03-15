552dcc4f0cf03dba562d7cd9f4384280e131446e
Sudarshan Hegde <suduhegde01@gmail.com>
2021-03-15 09:13:39 +0530

    added BMS class

--- checker.cpp
+++ checker.cpp
@@ -1,20 +1,80 @@
 #include <assert.h>
 #include <iostream>
+#include <string.h>
+#include <vector>
 using namespace std;
 
-bool batteryIsOk(float temperature, float soc, float chargeRate) {
-  if(temperature < 0 || temperature > 45) {
-    cout << "Temperature out of range!\n";
-    return false;
-  } else if(soc < 20 || soc > 80) {
-    cout << "State of Charge out of range!\n";
-    return false;
-  } else if(chargeRate > 0.8) {
-    cout << "Charge Rate out of range!\n";
-    return false;
-  }
-  return true;
-}
+typedef enum {
+    ENGLISH,
+    DEUTSCH
+} LanguageType;
+
+std::vector<std::string> BatteryParamNamesInEnglish{"Temperature" , "SOC" , "Charge Rate" };
+std::vector<std::string> BatteryParamNamesInDuetsch{ "Temperatur" , "Ladezustand" , "Laderate" };
+std::vector<std::vector<std::string>> BatteryParamNamesMasterList{ BatteryParamNamesInEnglish , BatteryParamNamesInDuetsch };
+
+
+enum BatteryParam{
+    Temperature,
+    SOC , 
+    ChargeRate,
+    NUM_OF_ELEM
+};
+
+
+typedef struct  {
+    float min;
+    float max;
+    float thresholdPercentage;
+}BatteryParamInfoType;
+
+BatteryParamInfoType TemperatureInfo =  { 0.0f,  45.0f, 5.0f };
+BatteryParamInfoType SocInfo =          { 20.0f, 80.0f, 5.0f };
+BatteryParamInfoType ChargingRateInfo = { 0.0f,  0.8f,  5.0f };
+
+std::vector<BatteryParamInfoType> BatteryParamInfo = { TemperatureInfo , SocInfo , ChargingRateInfo };
+
+
+class BMS {
+    
+private :
+    bool BatteryStatusIndicator = true; 
+    float m_temperature , m_soc, m_chargeRate;
+    LanguageType m_language; 
+public : 
+    BMS() {}
+
+
+
+    bool IsBatteryHealthOk()
+    {
+        checkParamInWorkingRange(m_temperature, BatteryParam::Temperature);
+        checkParamInWorkingRange(m_soc, BatteryParam::SOC);
+        checkParamInWorkingRange(m_chargeRate, BatteryParam::ChargeRate);
+        return BatteryStatusIndicator;
+    }
+
+    void checkParamInWorkingRange(float paramVal, int paramId)
+    {
+        if (paramVal < BatteryParamInfo[paramId].min)
+        {
+            BatteryStatusIndicator = false;
+            cout << BatteryParamNamesMasterList[m_language][paramId] << "LOW Breach!!!"<< endl; 
+        }
+        else if (paramVal > BatteryParamInfo[paramId].max)
+        {
+            BatteryStatusIndicator = false;
+            cout << BatteryParamNamesMasterList[m_language][paramId] << "HIGH Breach!!!" << endl;
+        }
+        else
+        {
+            BatteryStatusIndicator = true;
+            checkParamAtWarningLevels()
+        }
+    }
+
+
+};
 
 int main() {
   assert(batteryIsOk(25, 70, 0.7) == true);
